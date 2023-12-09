3a7e7400776f0cd9ad96920db40e9654a4434c78
Sebastian Rościszewski <sebastian@rosciszewski.pl>
2023-12-09 17:21:47 +0100

    Calico version changed to v3.26.4

--- control-plane.sh
+++ control-plane.sh
@@ -29,10 +29,9 @@ mkdir -p "$HOME"/.kube
 sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
 sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.3/manifests/tigera-operator.yaml
-curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.3/manifests/custom-resources.yaml -O
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.4/manifests/tigera-operator.yaml
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.4/manifests/custom-resources.yaml -O
 
 kubectl create -f custom-resources.yaml
 
 kubectl taint nodes --all node-role.kubernetes.io/control-plane-
-
