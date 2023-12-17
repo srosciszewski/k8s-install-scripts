637a6cec72fb4d0c96c0d5b1dafcdf6a1d15b251
Sebastian Rościszewski <sebastian@rosciszewski.pl>
2023-12-18 00:11:06 +0100

    New version Calico v3.27.0

--- control-plane.sh
+++ control-plane.sh
@@ -43,9 +43,9 @@ sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 
 # Install Claico Network Plugin Network
 
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.4/manifests/tigera-operator.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/tigera-operator.yaml
 
-curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.4/manifests/custom-resources.yaml -O
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/custom-resources.yaml -O
 
 kubectl create -f custom-resources.yaml
 
